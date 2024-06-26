0fdc7a203f5095124cc727f6baa6be1b27c1e0d8
parnianshamsa <153368555+parnianshamsa@users.noreply.github.com>
2024-06-26 19:12:12 +0330

    add ManagerValidatorClass

---
+++ ManagerValidator.java
@@ -0,0 +1,23 @@
+public class ManagerValidator {
+public boolean passwordValidate(String pass){
+    String password="parnian.dorsa84";
+    if (pass.equals(password)){
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+    public boolean usernameValidate(String pass){
+        String password="parnian.dorsa84";
+        if (pass.equals(password)){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+}
