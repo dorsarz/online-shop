014a7357db3b8711fdd384d79ffa568ef39a6a56
parnianshamsa <153368555+parnianshamsa@users.noreply.github.com>
2024-06-28 19:32:04 +0330

    Add files via upload

---
+++ LoginValidator.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.io.*;
+
+public class LoginValidator {
+    public boolean validPassword(String pass, File file, String usernameField) {
+        boolean flag = false;
+        try {
+            FileReader reader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] splitInformation = line.split(":");
+                if (usernameField.equals(splitInformation[2])) {
+
+                    if (pass.equals(splitInformation[3])) {
+                        flag = true;
+                    }
+                }
+                line = bufferedReader.readLine();
+
+            }
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        return flag;
+
+    }
+}
