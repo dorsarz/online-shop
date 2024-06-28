79bf7a2e0a14ce7c17591e5ddb012d9b68901203
dorsa rezaei <dorsarezaei24@gmail.com>
2024-06-28 16:28:33 +0330

    connection to file added

--- signIn.java
+++ signIn.java
@@ -2,6 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class signIn {
     private JFrame frame;
@@ -13,78 +18,87 @@ public class signIn {
     JTextField usernameField=new JTextField();
     JTextField passwordField=new JTextField();
     JTextField numberField=new JTextField();
+    JTextField addressField=new JTextField();
+    ArrayList<String> userInformation=new ArrayList<>();
+    File file=new File("information.txt");
     public signIn(JFrame frame,JPanel mainPanel){
         this.frame=frame;
         this.mainPanel=mainPanel;
     }
-    public void signInGUI(){
+    public void signInGUI() throws IOException {
         frame.repaint();
         frame.revalidate();
-        Font font=new Font("font",Font.BOLD,20);
-        JPanel signInPanel=new JPanel();
-        signInPanel.setBounds(0,0,1000,1000);
+        Font font = new Font("font", Font.BOLD, 20);
+        JPanel signInPanel = new JPanel();
+        signInPanel.setBounds(0, 0, 1000, 1000);
         signInPanel.setLayout(null);
         signInPanel.setBackground(Color.white);
-        backPanel.setBounds(200,120,700,600);
+        backPanel.setBounds(200, 120, 700, 600);
         backPanel.setLayout(null);
         backPanel.setBackground(Color.lightGray);
         signInPanel.add(backPanel);
 
-        JLabel firstNameLabel=new JLabel("First name:");
-        firstNameLabel.setBounds(40,10,200,50);
+        JLabel firstNameLabel = new JLabel("First name:");
+        firstNameLabel.setBounds(40, 10, 200, 50);
         firstNameLabel.setFont(font);
         backPanel.add(firstNameLabel);
 
 
-        firstNameField.setBounds(200,5,250,50);
+        firstNameField.setBounds(200, 5, 250, 50);
         backPanel.add(firstNameField);
+        userInformation.add(firstNameField.getText());
 
-        JLabel lastNameLabel=new JLabel("Last Name: ");
-        lastNameLabel.setBounds(40,60,200,50);
+        JLabel lastNameLabel = new JLabel("Last Name: ");
+        lastNameLabel.setBounds(40, 60, 200, 50);
         lastNameLabel.setFont(font);
         backPanel.add(lastNameLabel);
 
 
-        lastNameField.setBounds(200,55,250,50);
+        lastNameField.setBounds(200, 55, 250, 50);
         backPanel.add(lastNameField);
+        userInformation.add(lastNameField.getText());
 
-        JLabel username=new JLabel("User Name: ");
-        username.setBounds(40,110,200,100);
+        JLabel username = new JLabel("User Name: ");
+        username.setBounds(40, 110, 200, 100);
         username.setFont(font);
         backPanel.add(username);
 
 
-        usernameField.setBounds(200,105,250,100);
+        usernameField.setBounds(200, 105, 250, 100);
         backPanel.add(usernameField);
+        userInformation.add(usernameField.getText());
 
-        JLabel password=new JLabel("Password: ");
-        password.setBounds(40,210,200,100);
+        JLabel password = new JLabel("Password: ");
+        password.setBounds(40, 210, 200, 100);
         password.setFont(font);
         backPanel.add(password);
 
-        passwordField.setBounds(200,205,250,100);
+        passwordField.setBounds(200, 205, 250, 100);
         backPanel.add(passwordField);
+        userInformation.add(passwordField.getText());
 
-        JLabel phoneNumber=new JLabel("Phone Number: ");
+        JLabel phoneNumber = new JLabel("Phone Number: ");
         phoneNumber.setFont(font);
-        phoneNumber.setBounds(40,310,200,100);
+        phoneNumber.setBounds(40, 310, 200, 100);
         backPanel.add(phoneNumber);
 
-        numberField.setBounds(200,305,250,100);
+        numberField.setBounds(200, 305, 250, 100);
         backPanel.add(numberField);
+        userInformation.add(numberField.getText());
 
-        JLabel address=new JLabel("address: ");
+        JLabel address = new JLabel("address: ");
         address.setFont(font);
-        address.setBounds(40,410,200,100);
+        address.setBounds(40, 410, 200, 100);
         backPanel.add(address);
 
-        JTextField addressField=new JTextField();
-        addressField.setBounds(200,405,250,100);
-        backPanel.add(addressField);
 
-        JButton enter=new JButton("Enter");
+        addressField.setBounds(200, 405, 250, 100);
+        backPanel.add(addressField);
+        userInformation.add(addressField.getText());
+
+        JButton enter = new JButton("Enter");
         enter.setFont(font);
-        enter.setBounds(200,510,250,30);
+        enter.setBounds(200, 510, 250, 30);
 
 
         enter.addActionListener(new ActionListener() {
@@ -95,7 +109,7 @@ public class signIn {
                 backPanel.revalidate();
                 boxerror.setLayout(null);
                 boxerror.setBackground(Color.lightGray);
-                boxerror.setBounds(400,0,300,600);
+                boxerror.setBounds(400, 0, 300, 600);
                 setFullnameError();
                 setPhoneError();
                 SetpasswordError();
@@ -106,10 +120,9 @@ public class signIn {
             }
         });
         backPanel.add(enter);
-
-        JButton back=new JButton("Back");
+        JButton back = new JButton("Back");
         back.setFont(font);
-        back.setBounds(40,510,150,30);
+        back.setBounds(40, 510, 150, 30);
         backPanel.add(back);
         back.addActionListener(new ActionListener() {
             @Override
@@ -142,25 +155,25 @@ public class signIn {
             boxerror.repaint();
         }
         else {
-        if(phonecorrect){
-            if(!phonelength){
-                JLabel numberError=new JLabel("phoneNumber`s length must be 11");
+            if(phonecorrect){
+                if(!phonelength){
+                    JLabel numberError=new JLabel("phoneNumber`s length must be 11");
+                    numberError.setBounds(65,305,300,30);
+                    numberError.setFont(errorFont);
+                    boxerror.add(numberError);
+                    boxerror.revalidate();
+                    boxerror.repaint();
+                }
+
+            }
+            else {
+                JLabel numberError=new JLabel("This number is Invalid");
                 numberError.setBounds(65,305,300,30);
                 numberError.setFont(errorFont);
                 boxerror.add(numberError);
                 boxerror.revalidate();
                 boxerror.repaint();
-            }
-
-        }
-        else {
-            JLabel numberError=new JLabel("This number is Invalid");
-            numberError.setBounds(65,305,300,30);
-            numberError.setFont(errorFont);
-            boxerror.add(numberError);
-            boxerror.revalidate();
-            boxerror.repaint();
-        }}
+            }}
     }
 
     public void setFullnameError(){
@@ -184,24 +197,24 @@ public class signIn {
         }
         else {
 
-        if(namelength){
-            if (!namecorrectness){
-                JLabel error=new JLabel("Your firstname must contains Alphabet only!");
+            if(namelength){
+                if (!namecorrectness){
+                    JLabel error=new JLabel("Your firstname must contains Alphabet only!");
+                    error.setBounds(65,5,350,30);
+                    error.setFont(errorFont);
+                    boxerror.add(error);
+                    boxerror.revalidate();
+                    boxerror.repaint();
+                }
+            }
+            else {
+                JLabel error=new JLabel("Your firstname`s length must be between 3-12!");
                 error.setBounds(65,5,350,30);
                 error.setFont(errorFont);
                 boxerror.add(error);
                 boxerror.revalidate();
                 boxerror.repaint();
-            }
-        }
-        else {
-            JLabel error=new JLabel("Your firstname`s length must be between 3-12!");
-            error.setBounds(65,5,350,30);
-            error.setFont(errorFont);
-            boxerror.add(error);
-            boxerror.revalidate();
-            boxerror.repaint();
-        }}
+            }}
         if(lastlen==0){
             JLabel numberError=new JLabel("Please fill in the blank! ");
             numberError.setBounds(65, 55, 350, 30);
@@ -269,13 +282,17 @@ public class signIn {
         }
     }
     public void SetAdressError(){
+        int addressLength=addressField.getText().length();
         Font errorFont = new Font("font", Font.ITALIC, 10);
-        JLabel numberError=new JLabel("Please fill in the blank! ");
-        numberError.setBounds(65, 410, 350, 30);
-        numberError.setFont(errorFont);
-        boxerror.add(numberError);
-        boxerror.revalidate();
-        boxerror.repaint();
+        if(addressLength==0) {
+            JLabel numberError = new JLabel("Please fill in the blank! ");
+            numberError.setBounds(65, 410, 350, 30);
+            numberError.setFont(errorFont);
+            boxerror.add(numberError);
+            boxerror.revalidate();
+            boxerror.repaint();
+        }
     }
 
 }
+
